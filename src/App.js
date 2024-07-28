a189907747d62d3cdc62dfde173e5843db33d8c6
Abhiroop <127959479+Abhiroop17@users.noreply.github.com>
2024-07-28 14:51:41 +0000

    Day 01

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Routes, Route } from 'react-router-dom';
+function App() {
+  return (
+    <Routes>
+      {/* <Route path="/" element={<Homepage/>} />
+      <Route path="/movie/:id" element= {<moviepage/>} />
+      <Route path="/plays" element= {<plays/>} /> */}  
+    </Routes>
+  );
+}
+
+export default App;
